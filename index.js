e837c38a2ba5bbe223c9f1f0e63eb58328fdbd28
Edmund Yu <eyu1@ualberta.ca>
2021-09-24 02:35:05 +0000

    Implemented index.js and iss.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+const { nextISSTimesForMyLocation } = require("./iss");
+
+let formatTimes = function (object) {
+  for (let obj of object) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(obj.risetime);
+    const duration = obj.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  console.log(formatTimes(passTimes));
+});
